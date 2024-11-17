7de962add427213e6a974a8f61262051968ffa85
Dervis Acar <12303993@student.pxl.be>
2024-11-17 19:42:27 +0100

    MasterMind-04

--- MasterMind/MasterMind/MainWindow.xaml.cs
+++ MasterMind/MasterMind/MainWindow.xaml.cs
@@ -21,8 +21,16 @@ namespace MasterMind
     /// </summary>
     public partial class MainWindow : Window
     {
-        string[] kleuren = {"Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw"};
+        string[] kleurenArray = {"Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw"};
         string[] code;
+        Dictionary<string, SolidColorBrush> kleurenDictionary = new Dictionary<string, SolidColorBrush> { 
+            { "Rood", new SolidColorBrush(Colors.Red) }, 
+            { "Geel", new SolidColorBrush(Colors.Yellow) },
+            { "Oranje", new SolidColorBrush(Colors.Orange) },
+            { "Wit", new SolidColorBrush(Colors.White) },
+            { "Groen", new SolidColorBrush(Colors.Green) },
+            { "Blauw", new SolidColorBrush(Colors.Blue) },
+        };
 
         public MainWindow()
         {
@@ -39,11 +47,11 @@ namespace MasterMind
 
             while (i<4) 
             {
-                int rng = random.Next(0, kleuren.Length);
+                int rng = random.Next(0, kleurenArray.Length);
                 
                 if (!alGebruikteRngs.Contains(rng))
                 {
-                    code[i] = kleuren[rng];
+                    code[i] = kleurenArray[rng];
                     alGebruikteRngs.Add(rng);
                     i++;
                 }
@@ -54,13 +62,50 @@ namespace MasterMind
         public void VulCbo()
         {
             int i = 0;
-            while (i < kleuren.Length)
+            while (i < kleurenArray.Length)
             {
-                Cbo1.Items.Add(kleuren[i]);
-                Cbo2.Items.Add(kleuren[i]);
-                Cbo3.Items.Add(kleuren[i]);
-                Cbo4.Items.Add(kleuren[i]);
+                Cbo1.Items.Add(kleurenArray[i]);
+                Cbo2.Items.Add(kleurenArray[i]);
+                Cbo3.Items.Add(kleurenArray[i]);
+                Cbo4.Items.Add(kleurenArray[i]);
                 i++;
+            }
+        }
+
+
+        private void Cbo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(Cbo1.SelectedIndex != -1)
+            {
+                string geselecteerdeKleur = Cbo1.SelectedItem.ToString();
+                Lbl1.Background = kleurenDictionary[geselecteerdeKleur];
+            }
+        }
+
+        private void Cbo2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Cbo2.SelectedIndex != -1)
+            {
+                string geselecteerdeKleur = Cbo2.SelectedItem.ToString();
+                Lbl2.Background = kleurenDictionary[geselecteerdeKleur];
+            }
+        }
+
+        private void Cbo3_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Cbo3.SelectedIndex != -1)
+            {
+                string geselecteerdeKleur = Cbo3.SelectedItem.ToString();
+                Lbl3.Background = kleurenDictionary[geselecteerdeKleur];
+            }
+        }
+
+        private void Cbo4_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Cbo4.SelectedIndex != -1)
+            {
+                string geselecteerdeKleur = Cbo4.SelectedItem.ToString();
+                Lbl4.Background = kleurenDictionary[geselecteerdeKleur];
             }
         }
     }
